0d6e7a822d55b4aa6772c5ab28c507312765b41b
korbinschulz <korbkris@gmail.com>
2023-10-21 18:23:22 -0500

    added nav and home buttons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginComponent from './utils/LoginComponent';
 import SignUpComponent from "./utils/SignupComponent";
+import Nav from "./components/Nav/Nav";
+import "./App.css";
+import Home from "./containers/Home/Home";
+
 const App = () => {
   return (
     <div className="app-outer">
       <Router>
+      <Nav />
         <Routes>
           <Route path='/' element={<LoginComponent />} />
           <Route path ='/signUp' element={<SignUpComponent />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </Router>
     </div>
